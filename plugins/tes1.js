3803763c25068a0309ff124adcc9304ad714c05c
Naaazzzzz <108647595+AnandaGTPS@users.noreply.github.com>
2024-03-03 07:11:20 +0700

    Upload file via SL-Botz

---
+++ plugins/tes1.js
@@ -0,0 +1,21 @@
+* Creator*:  Naaazzzzz
+* Contact*:  wa.me/6282139672290
+* Community*:  https://chat.whatsapp.com/Ftk1hl3l84EBSHI2mF7Dw3
+* Message*: Sorry bre masih dikit
+* Total*: 14
+
+ List Plugins 
+*×* ai-dalle : 1.0.0
+*×* ai-lexica : 1.0.0
+*×* ai-prodia : 1.0.0
+*×* ai-simsimi : 1.0.0
+*×* ai-text2img : 1.0.0
+*×* m3 : 1.0.0
+*×* main : 1.0.2
+*×* p : 1.0.0
+*×* p3 : 1.0.0
+*×* tes : 1.0.0
+*×* tools-modify : 1.0.0
+*×* tools-obfuscator : 1.0.0
+*×* tools-poll : 1.0.0
+*×* tools-remini : 1.0.0
